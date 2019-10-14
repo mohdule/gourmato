16b3c9a31e32b1e13402a7ce3ca65ff0b954e8ac
Mohannad Salah <mohdule117@gmail.com>
2019-10-14 23:10:58 +0200

    Wrapped the app with react-redux provider and react-router-dom browser router components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
 import App from './App';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Route component={App} />
+    </Router>
+  </Provider>, document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
