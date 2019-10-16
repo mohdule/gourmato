71e9098ea626758b7e05f39eb8f180b97f5445d1
Mohannad Salah <mohdule117@gmail.com>
2019-10-16 09:43:10 +0200

    Created restaurant search logic inside the retaurants reducer module and it's saga

--- src/store/modules/restaurants.js
+++ src/store/modules/restaurants.js
@@ -6,6 +6,10 @@ export const TYPES = {
   FIND_RESTAURANTS: 'App/restaurants/FIND_RESTAURANTS',
   FOUND_RESTAURANTS: 'App/restaurants/FOUND_RESTAURANTS',
   FIND_RESTAURANTS_FAIL: 'App/restaurants/FIND_RESTAURANTS_FAIL',
+
+  SEARCH_FOR_RESTAURANT: 'App/restaurants/SEARCH_FOR_RESTAURANT',
+  SEARCH_FOR_RESTAURANT_FAIL: 'App/restaurants/SEARCH_FOR_RESTAURANT_FAIL',
+  SEARCH_FOR_RESTAURANT_SUCCESS: 'App/restaurants/SEARCH_FOR_RESTAURANT_SUCCESS',
 };
 
 // --------|
@@ -16,6 +20,11 @@ const initialState = {
   loading: false,
   error: '',
   all: [],
+  suggestions: {
+    loading: false,
+    error: '',
+    all: [],
+  },
 };
 // => reducer
 export default function (state = initialState, { type, payload }) {
@@ -38,6 +47,33 @@ export default function (state = initialState, { type, payload }) {
         error: '',
         all: [],
       };
+    case TYPES.SEARCH_FOR_RESTAURANT:
+      return {
+        ...state,
+        suggestions: {
+          loading: true,
+          error: '',
+          all: [],
+        },
+      };
+    case TYPES.SEARCH_FOR_RESTAURANT_SUCCESS:
+      return {
+        ...state,
+        suggestions: {
+          loading: false,
+          all: payload,
+          error: '',
+        },
+      };
+    case TYPES.SEARCH_FOR_RESTAURANT_FAIL:
+      return {
+        ...state,
+        suggestions: {
+          loading: false,
+          all: [],
+          error: payload,
+        },
+      };
     case LocationActions.SET_LOCATION:
       return initialState;
     default:
@@ -48,6 +84,23 @@ export default function (state = initialState, { type, payload }) {
 // ----------------|
 // Action Creators |
 // ----------------|
+// Search section #1 action creators
+export const searchForRestaurant = (name) => ({
+  type: TYPES.SEARCH_FOR_RESTAURANT,
+  payload: name,
+});
+
+export const foundRestaruantSearchResults = (restaurants) => ({
+  type: TYPES.SEARCH_FOR_RESTAURANT_SUCCESS,
+  payload: restaurants,
+});
+
+export const failedSearchingRestaurant = (errorMsg) => ({
+  type: TYPES.SEARCH_FOR_RESTAURANT_FAIL,
+  payload: errorMsg,
+});
+
+// Search section #2 action creators
 export const loadRestaurants = (categoryIds, cuisinesIds, entityId, entityType) => ({
   type: TYPES.FIND_RESTAURANTS,
   payload: {
--- src/store/sagas/restaurants-saga.js
+++ src/store/sagas/restaurants-saga.js
@@ -1,8 +1,44 @@
-import { put, takeLatest } from 'redux-saga/effects';
+import { put, takeLatest, select } from 'redux-saga/effects';
 import * as restaurantsModule from '../modules/restaurants';
 import axios from '../../axios';
 
-function* findRestaurant({ payload }) {
+const normalizeResturants = (restaurants) => restaurants.map(({ restaurant }) => ({
+  id: restaurant.id,
+  name: restaurant.name,
+  rating: restaurant.user_rating.aggregate_rating,
+  highlights: restaurant.highlights,
+  phone_numbers: restaurant.phone_numbers,
+  address: restaurant.location.address,
+  locality: restaurant.location.locality,
+  locality_verbose: restaurant.location.locality_verbose,
+  cuisines: restaurant.cuisines,
+  thumb_url: restaurant.thumb,
+  menu_url: restaurant.menu_url,
+  photos_url: restaurant.photos_url,
+  currency: restaurant.currency,
+  average_cost_for_two: restaurant.average_cost_for_two,
+  price_range: restaurant.price_range,
+}));
+
+function* searchByName({ payload }) {
+  try {
+    const {
+      entity_id: entityId,
+      entity_type: entityType,
+    } = yield select((state) => state.location.current);
+    const response = yield axios.get(`/search?entity_id=${entityId}&entity_type=${entityType}&q=${payload}`);
+    const restaurantsFound = normalizeResturants(response.data.restaurants);
+    const suggestions = restaurantsFound.map((restaurant) => ({
+      title: restaurant.id,
+      ...restaurant,
+    }));
+    yield put(restaurantsModule.foundRestaruantSearchResults(suggestions));
+  } catch (error) {
+    yield put(restaurantsModule.failedSearchingRestaurant('Unable to find restaurant'));
+  }
+}
+
+function* findRestaurants({ payload }) {
   const {
     entityId, entityType, cuisinesIds, categoryIds,
   } = payload;
@@ -14,23 +50,7 @@ function* findRestaurant({ payload }) {
     );
 
     // Normalizing the restuarants list
-    const restaurants = response.data.restaurants.map(({ restaurant }) => ({
-      id: restaurant.id,
-      name: restaurant.name,
-      rating: restaurant.user_rating.aggregate_rating,
-      highlights: restaurant.highlights,
-      phoneNumbers: restaurant.phone_numbers,
-      address: restaurant.location.address,
-      locality: restaurant.location.locality,
-      localityVerbose: restaurant.location.locality_verbose,
-      cuisines: restaurant.cuisines,
-      thumb_url: restaurant.thumb,
-      menuUrl: restaurant.menu_url,
-      photosUrl: restaurant.photos_url,
-      currency: restaurant.currency,
-      averageCostForTwo: restaurant.average_cost_for_two,
-      priceRange: restaurant.price_range,
-    }));
+    const restaurants = normalizeResturants(response.data.restaurants);
     yield put(restaurantsModule.loadedRestaurants(restaurants));
   } catch (error) {
     yield put(restaurantsModule.failedLoadingRestaurants('Unable to search for restaurants, try again'));
@@ -38,5 +58,6 @@ function* findRestaurant({ payload }) {
 }
 
 export default function* () {
-  yield takeLatest(restaurantsModule.TYPES.FIND_RESTAURANTS, findRestaurant);
+  yield takeLatest(restaurantsModule.TYPES.FIND_RESTAURANTS, findRestaurants);
+  yield takeLatest(restaurantsModule.TYPES.SEARCH_FOR_RESTAURANT, searchByName);
 }
