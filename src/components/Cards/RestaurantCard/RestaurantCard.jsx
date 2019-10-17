b8f0e187c7d332a0688048cf4faa89ab986abe07
Mohannad Salah <mohdule117@gmail.com>
2019-10-17 10:17:46 +0200

    Removed unused import

--- src/components/Cards/RestaurantCard/RestaurantCard.jsx
+++ src/components/Cards/RestaurantCard/RestaurantCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Card, Rating, Image, Grid, Header, Icon, List,
+  Card, Rating, Image, Grid, Header, List,
 } from 'semantic-ui-react';
 
 import './restaurantCard.css';
